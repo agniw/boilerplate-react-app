aea505e587e9879354239eaa4d13c2b692ee7e9c
Agnieszka Wojsznis <agnieszka.wojsznis@protonmail.com>
2019-01-29 14:48:45 +0100

    initial commit

---
+++ src/components/Root.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Root extends React.Component {
+    render(){
+        return (
+            <div>
+                <p> Hello World. </p>
+            </div>
+        )
+    }
+}
+
+export default Root;
